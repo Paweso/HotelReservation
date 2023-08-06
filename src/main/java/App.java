9f6682da8fa83f3462c0a70ab2b5ebc10f4e3fcf
paweso <paweso@wp.pl>
2023-08-06 12:59:03 +0200

    adding a welcome text

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Witaj w systemie rezerwacji dla hotelu Overlook");
+        System.out.print("Aktualna wersja systemu: ");
+        System.out.println(1);
+        System.out.print("Wersja developerska: ");
+        System.out.println(true);
     }
 }
